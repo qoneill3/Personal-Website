63f800d1ad3f0d6f544be64617f6b138b5bb2dd4
Quinn O'Neill <quinnoneill@Quinns-MBP-2.domain>
2018-03-26 22:20:14 -0600

    got public files sent(including css/js)to front end. Good reactive appearance. Bio needs work

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 var app = express();
 
-app.use(express.static(__dirname+'./root/css'));
+app.use(express.static(__dirname+'/root/public'));
 
 app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
@@ -17,15 +17,15 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.set('index', path.join(__dirname, 'index'));
+app.use(session({secret: "portfolio"}));
 
-app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 app.use(flash());
 
-app.use(express.static(__dirname + 'root/css'));
-
 app.get('/', function homepage (req, res) {
- 	res.render(__dirname + 'root/index.html');
+ 	res.render(__dirname + '/root/views/index.ejs');
  });
 
 app.listen(process.env.PORT || 3000, function () {
